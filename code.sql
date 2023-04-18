0ec5fae88aa340bfc1bab3914e332a51127b3dae
Barsha Chaudhary <64405568+BarshaC@users.noreply.github.com>
2023-04-18 12:57:37 -0400

    Updated sql for the Task 10, 11 and 12
    Changed Grades to Grade
    Changed the edge case to total percentage to be 100% even when updating an assignment to a course

--- code.sql
+++ code.sql
@@ -1,122 +1,160 @@
+CREATE DATABASE GradeBook;
+USE GradeBook;
+
 -- Create Course table
 CREATE TABLE Course (
-  course_id INT PRIMARY KEY,
-  department VARCHAR(255),
-  course_number INT,
-  course_name VARCHAR(255),
-  semester VARCHAR(255),
-  year INT
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  department VARCHAR(255) NOT NULL,
+  course_number INT NOT NULL,
+  course_name VARCHAR(255) NOT NULL,
+  semester VARCHAR(255) NOT NULL,
+  year_ INT NOT NULL
 );
 
--- Create Student table
 CREATE TABLE Student (
-  student_id INT PRIMARY KEY,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
-  course_id INT,
-  FOREIGN KEY (course_id) REFERENCES Course(course_id)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  course_id INT NOT NULL,
+  FOREIGN KEY (course_id) REFERENCES Course(id)
 );
 
 -- Create Assignment table
 CREATE TABLE Assignment (
-  assignment_id INT PRIMARY KEY,
-  category VARCHAR(255),
-  percentage DECIMAL(5,2),
-  course_id INT,
-  FOREIGN KEY (course_id) REFERENCES Course(course_id)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  category VARCHAR(255) NOT NULL,
+  percentage DECIMAL(5,2) NOT NULL,
+  course_id INT NOT NULL,
+  FOREIGN KEY (course_id) REFERENCES Course(id)
 );
 
--- Create Grades table
-CREATE TABLE Grades (
-  grade_id INT PRIMARY KEY,
-  assignment_id INT,
-  student_id INT,
-  score DECIMAL(5,2),
-  FOREIGN KEY (assignment_id) REFERENCES Assignment(assignment_id),
-  FOREIGN KEY (student_id) REFERENCES Student(student_id)
+-- Create Grade table
+CREATE TABLE Grade (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  assignment_id INT NOT NULL,
+  student_id INT NOT NULL,
+  score DECIMAL(5,2) NOT NULL,
+  FOREIGN KEY (assignment_id) REFERENCES Assignment(id),
+  FOREIGN KEY (student_id) REFERENCES Student(id)
 );
 
 
 -- Insert values into Course table
-INSERT INTO Course (course_id, department, course_number, course_name, semester, year)
+INSERT INTO Course (id, department, course_number, course_name, semester, year_)
 VALUES (1, 'Computer Science', 101, 'Intro to Computer Science', 'Fall', 2022);
 
 -- Insert values into Student table
-INSERT INTO Student (student_id, first_name, last_name, course_id)
+INSERT INTO Student (id, first_name, last_name, course_id)
 VALUES (1, 'John', 'Doe', 1),
        (2, 'Jane', 'Smith', 1),
        (3, 'David', 'Johnson', 1);
 
 -- Insert values into Assignment table
-INSERT INTO Assignment (assignment_id, category, percentage, course_id)
+INSERT INTO Assignment (id, category, percentage, course_id)
 VALUES (1, 'Participation', 10.0, 1),
        (2, 'Homework', 20.0, 1),
        (3, 'Tests', 50.0, 1),
        (4, 'Projects', 20.0, 1);
 
 -- Insert values into Grades table
-INSERT INTO Grades (grade_id, assignment_id, student_id, score)
-VALUES (1, 1, 1, 8.5),
-       (2, 2, 1, 92.0),
-       (3, 3, 1, 75.0),
-       (4, 4, 1, 88.0),
-       (5, 1, 2, 9.0),
-       (6, 2, 2, 88.5),
-       (7, 3, 2, 82.0),
-       (8, 4, 2, 95.0),
-       (9, 1, 3, 7.0),
-       (10, 2, 3, 78.0),
-       (11, 3, 3, 90.0),
-       (12, 4, 3, 91.5);
+INSERT INTO Grade (id, assignment_id, student_id, score)
+VALUES
+(1, 1, 1, 8.5),
+(2, 2, 1, 92.0),
+(3, 3, 1, 75.0),
+(4, 4, 1, 88.0),
+(5, 1, 2, 9.0),
+(6, 2, 2, 88.5),
+(7, 3, 2, 82.0),
+(8, 4, 2, 95.0),
+(9, 1, 3, 7.0),
+(10, 2, 3, 78.0),
+(11, 3, 3, 90.0),
+(12, 4, 3, 91.5);
 
 SELECT * FROM Course;
 SELECT * FROM Student;
 SELECT * FROM Assignment;
-SELECT * FROM Grades;
-
-SELECT s.* FROM Student s
-JOIN Course c ON s.course_id = c.course_id
-WHERE c.course_id = 1;
+SELECT * FROM Grade;
 
 -- Average score
 SELECT AVG(score) AS average_score
-FROM Grades
+FROM Grade
 WHERE assignment_id = 2;
 
 -- Highest score
 SELECT MAX(score) AS highest_score
-FROM Grades
+FROM Grade
 WHERE assignment_id = 2;
 
 -- Lowest score
 SELECT MIN(score) AS lowest_score
-FROM Grades
+FROM Grade
 WHERE assignment_id = 2;
 
+-- List all of the students in a given course
+SELECT s.* FROM Student s
+JOIN Course c ON s.course_id = c.id
+WHERE c.id = 1;
 
-INSERT INTO Assignment (assignment_id, category, percentage, course_id)
-VALUES (5, 'Final Exam', 30.0, 1);
 
+-- List all of the students in a course and all of their scores on every assignment;
+SELECT s.first_name, s.last_name, a.category, g.score
+FROM Student s
+JOIN Grade g ON s.id = g.student_id
+JOIN Assignment a ON g.assignment_id = a.id
+WHERE s.course_id = 1;
+
+-- Add an assignment to a course
+INSERT INTO Assignment (category, percentage, course_id)
+VALUES ('Final Exam', 30, 1);
+
+-- Show the changes in the assignment
+SELECT * FROM Assignment;
+
+-- Change the percentages of the categories for a course;
 UPDATE Assignment
-SET percentage = 30.0
-WHERE category = 'Homework' AND course_id = 1;
+SET percentage = 5 WHERE id = 1 AND course_id = 1;
+UPDATE Assignment
+SET percentage = 20 WHERE id = 2 AND course_id = 1;
+UPDATE Assignment
+SET percentage = 30 WHERE id = 3 AND course_id = 1;
+UPDATE Assignment
+SET percentage = 15 WHERE id = 4 AND course_id = 1;
 
-UPDATE Grades
+-- Show the changes to the percentage categories for a course
+SELECT * FROM Assignment WHERE course_id = 1;
+
+-- Scores before the update of 2 points
+SELECT * FROM Grade WHERE assignment_id = 3;
+
+
+-- Add 2 points to the score of each student on an assignment;
+UPDATE Grade
 SET score = score + 2
 WHERE assignment_id = 3;
 
-Add 2 points to the score of students whose last name contains a 'Q':
+-- Scores before the update
+SELECT * FROM Grade WHERE assignment_id = 3;
 
+-- Add 2 points to the score of students whose last name contains a 'Q'
+UPDATE Grade g
+JOIN Student s ON s.id = g.student_id
+SET score = score + 2
+WHERE s.last_name LIKE '%Q%';
+
+-- Compute the grade for a student
 SELECT s.first_name, s.last_name, SUM(a.percentage * g.score / 100) AS grade
 FROM Student s
-JOIN Grades g ON s.student_id = g.student_id
-JOIN Assignment a ON g.assignment_id = a.assignment_id
-GROUP BY s.first_name, s.last_name
-HAVING s.student_id = 1;
+JOIN Grade g ON s.id = g.student_id
+JOIN Assignment a ON g.assignment_id = a.id
+WHERE s.id = 1
+GROUP BY s.first_name, s.last_name;
 
-SELECT s.first_name, s.last_name, SUM(a.percentage * (g.score - MIN(g.score)) / 100) AS grade
+-- Compute the grade for a student, where the lowest score for a given category is dropped.
+SELECT s.first_name, s.last_name, 
+  (SUM(g.score) - MIN(g.score)) / (COUNT(g.score) - 1) AS grade
 FROM Student s
-JOIN Grades g ON s.student_id = g.student_id
-JOIN Assignment a ON g.assignment_id = a.assignment_id
-GROUP BY s.first_name, s.last_name
-HAVING s.student_id = 2;
+JOIN Grade g ON s.id = g.student_id
+WHERE s.id = 1
+GROUP BY s.first_name, s.last_name;
